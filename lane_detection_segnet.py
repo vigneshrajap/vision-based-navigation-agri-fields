9e071b705790651af5e606c2adbce82b4c8683a5
Vignesh <vignesh.raja199@gmail.com>
2019-09-19 14:36:42 +0200

    arguments added for only prediction and first set of predicted outputs folder added here for now

--- lane_detection_segnet.py
+++ lane_detection_segnet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import keras_segmentation
 
-model = keras_segmentation.models.segnet.segnet(n_classes=51,  input_height=360, input_width=640  )
+model = keras_segmentation.models.segnet.segnet(n_classes=3,  input_height=360, input_width=640)
 
 model.train(
     train_images =  "~/Frogn_Dataset/images_prepped_train/",
@@ -9,10 +9,17 @@ model.train(
     checkpoints_path = "/tmp/vgg_segnet" , epochs=5
 )
 
+#model.load_weights('/content/drive/My Drive/Colab Notebooks/segnet_weights/segnet.4') # load json and create model
+
 out = model.predict_segmentation(
-    inp="~/Frogn_Dataset/images_prepped_test/frogn_10008.png", 
+    inp="~/Frogn_Dataset/images_prepped_test/frogn_10008.png",
     out_fname="out.png"
 )
 
 import matplotlib.pyplot as plt
 plt.imshow(out)
+
+#out = model.predict_multiple(
+    #inp_dir="~/Frogn_Dataset/images_prepped_test/",
+    #out_dir="~/predicted_outputs/"
+#)
