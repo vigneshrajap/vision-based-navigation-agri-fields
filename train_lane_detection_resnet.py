0f4fb981163b30a06b3383c57b4aba507c70acf0
Marianne <marianne.bakken@sintef.no>
2019-09-25 12:40:56 +0200

    Training setup on sintef server

--- train_lane_detection_resnet.py
+++ train_lane_detection_resnet.py
@@ -2,16 +2,21 @@
 import keras_segmentation
 import os
 
+setup_name = 'resnet'
+
 data_folder = 'Frogn_Dataset'
 model_folder = 'models'
 
-model = keras_segmentation.models.segnet.segnet(n_classes=4,  input_height=360, input_width=640  )
+model = keras_segmentation.models.segnet.resnet50_segnet(n_classes=4,  input_height=320, input_width=640  )
 
 model.summary()
 
 model.train(
     train_images =  os.path.join(data_folder,'images_prepped_train'),
     train_annotations = os.path.join(data_folder,'annotations_prepped_train'),
-    checkpoints_path = os.path.join(model_folder,'segnet_from_scratch_v2'), epochs=50
+    checkpoints_path = os.path.join(model_folder,setup_name),
+    epochs=50,
+    
+    
 )
 
