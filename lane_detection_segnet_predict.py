c3cceb0167314f8c85257eb05eb88061b39e248b
Marianne <marianne.bakken@sintef.no>
2019-09-26 10:50:11 +0200

    Fixed prediction function for multiple files, and added training setup for resnet on SINTEF server.

--- lane_detection_segnet_predict.py
+++ lane_detection_segnet_predict.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python
 import keras_segmentation
 import os
-import matplotlib.pyplot as plt
-data_folder = 'Frogn_Dataset'
-model_folder = 'models'
+import glob
+import argparse
 
-model = keras_segmentation.models.segnet.segnet(n_classes=3, input_height=360, input_width=640  )
-model.load_weights(os.path.join(model_folder,'segnet_from_scratch_v1.4')) # load json and create model
-
+'''
 out = model.predict_segmentation(
     inp=os.path.join(data_folder,'images_prepped_test/frogn_10008.png'), 
     out_fname="out.png"
 )
 print('out shape',out.shape)
 print(out)
-'''
+
 #Run from command line:
 python -m keras_segmentation predict \
  --checkpoints_path="/home/marianne/Code/vision-based-navigation-agri-fields/models/segnet_from_scatch.0" \
  --input_path="/home/marianne/Code/vision-based-navigation-agri-fields/Frogn_Dataset/images_prepped_test" \
  --output_path="."
- '''
+'''
+#out = keras_segmentation.predict.predict_multiple( model=None , inps=None , inp_dir=None, out_dir=None , checkpoints_path=None  )
+def main():
+    parser = argparse.ArgumentParser(description="Run evaluation of model on a set of images and annotations.")
+    parser.add_argument("--model_prefix", default = '', help = "Prefix of model filename")
+    parser.add_argument("--output_folder",default = '', help = "Relative path output folder")
+    parser.add_argument("--data_folder", default = 'Frogn_Dataset', help = "Relative path of data folder")
+    parser.add_argument("--model_folder", default = 'models', help = "Relative path of model folder")
+    parser.add_argument("--images_folder",default = 'images_prepped_test', help = "Name of image folder")
+
+    args = parser.parse_args()
+    
+    model_path = os.path.join(args.model_folder,args.model_prefix)
+    im_dir = os.path.join(args.data_folder,args.images_folder)
+    output_folder = args.output_folder
+
+    keras_segmentation.predict.predict_multiple(model=None , inps=None, inp_dir = im_dir,  out_dir = output_folder, checkpoints_path=model_path)
+
+if __name__ == "__main__":
+    main()
--- train_lane_detection_resnet.py
+++ train_lane_detection_resnet.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
+import sys
+sys.path.insert(1, '../image-segmentation-keras') #add 
 import keras_segmentation
 import os
 
-setup_name = 'resnet'
+
+
+
+setup_name = 'resnet_3class_test'
 
 data_folder = 'Frogn_Dataset'
 model_folder = 'models'
 
-model = keras_segmentation.models.segnet.resnet50_segnet(n_classes=4,  input_height=320, input_width=640  )
+model = keras_segmentation.models.segnet.resnet50_segnet(n_classes=3,  input_height=320, input_width=640  )
 
 model.summary()
 
@@ -16,7 +21,7 @@ model.train(
     train_annotations = os.path.join(data_folder,'annotations_prepped_train'),
     checkpoints_path = os.path.join(model_folder,setup_name),
     epochs=50,
-    
-    
+    auto_resume_checkpoint = False,
+    logging = True
 )
 
