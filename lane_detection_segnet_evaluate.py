b237e693c1bd34236884bdb4634baf5f0446e3f4
Marianne <marianne.bakken@sintef.no>
2019-09-27 12:01:59 +0200

    Added visualization in evaluation function.

--- lane_detection_segnet_evaluate.py
+++ lane_detection_segnet_evaluate.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-import keras_segmentation
 import os
 import glob
 import matplotlib.pyplot as plt
+import sys
+sys.path.insert(1, '../image-segmentation-keras')
 from keras_segmentation.data_utils.data_loader import get_image_arr , get_segmentation_arr
 from keras_segmentation.predict import predict, model_from_checkpoint_path
 from keras_segmentation import metrics
@@ -11,11 +12,11 @@ import numpy as np
 from tqdm import tqdm 
 import argparse
 
-def evaluate( model=None , inp_images=None , annotations=None , checkpoints_path=None ):
+def evaluate( model=None , inp_images=None , annotations=None , checkpoints_path=None, epoch = None, visualize = False, output_folder = ''):
     #Finished implementation of the evaluate function in keras_segmentation.predict
     #Input: array of paths or nd arrays
     if model is None and ( not checkpoints_path is None ):
-        model = model_from_checkpoint_path(checkpoints_path)
+        model = model_from_checkpoint_path(checkpoints_path,epoch)
         
     ious = []
     for inp , ann   in tqdm(zip( inp_images , annotations )):
@@ -24,6 +25,35 @@ def evaluate( model=None , inp_images=None , annotations=None , checkpoints_path
         gt = gt.argmax(-1)
         iou = metrics.get_iou( gt , pr , model.n_classes )
         ious.append( iou )
+        
+        if visualize:
+            print(iou)
+            print('gt',np.min(gt),np.max(gt))
+            print('pr',np.min(pr),np.max(pr))
+            fig = plt.figure()
+            plt.title("IOU:"+str(iou))
+            ax1 = fig.add_subplot(2,2,1)
+            ax1.imshow(gt-pr)
+            #ax1.colorbar()
+            ax1.title.set_text("Difference GT-pred")
+            
+            ax2 = fig.add_subplot(2,2,2)
+            ax2.imshow(gt)
+            ax2.title.set_text('GT')
+            
+            ax3 = fig.add_subplot(2,2,3)
+            ax3.imshow(pr)
+            ax3.title.set_text('pred')
+            
+            ax4 = fig.add_subplot(2,2,4)
+            ax4.imshow(plt.imread(inp))
+            ax4.title.set_text('Input image')
+            #plt.show()
+            #print(checkpoints_path, inp)
+            if not output_folder:
+                plt.savefig(checkpoints_path+'_IOU_'+os.path.basename(inp))
+            else: 
+                plt.savefig(os.path.join(output_folder,os.path.basename(checkpoints_path)+'_IOU_'+os.path.basename(inp)))
     ious = np.array( ious )
     print("Class wise IoU "  ,  np.mean(ious , axis=0 ))
     print("Total  IoU "  ,  np.mean(ious ))
@@ -31,10 +61,13 @@ def evaluate( model=None , inp_images=None , annotations=None , checkpoints_path
 def main():
     parser = argparse.ArgumentParser(description="Run evaluation of model on a set of images and annotations.")
     parser.add_argument("--model_prefix", default = '', help = "Prefix of model filename")
+    parser.add_argument("--epoch", default = None, help = "Checkpoint epoch number")
     parser.add_argument("--data_folder", default = 'Frogn_Dataset', help = "Relative path of data folder")
     parser.add_argument("--model_folder", default = 'models', help = "Relative path of model folder")
     parser.add_argument("--images_folder",default = 'images_prepped_test', help = "Name of image folder")
     parser.add_argument("--annotations_folder",default = 'annotations_prepped_test', help = "Name of annotations folder")
+    parser.add_argument("--visualize",default = False, help = "Turn visualization on/off")
+    parser.add_argument("--output_folder",default = '', help = "Ouput folder for figures.")
     args = parser.parse_args()
     
     model_path = os.path.join(args.model_folder,args.model_prefix)
@@ -42,8 +75,7 @@ def main():
     ann_files = glob.glob(os.path.join(args.data_folder,args.annotations_folder , '*.png'))
     
     print('Running evaluation on ',len(im_files),'images, and',len(ann_files),'annotations:')
-    print('Images: ', im_files, 'Annotations:', ann_files) 
-    evaluate(model=None , inp_images= im_files , annotations= ann_files, checkpoints_path=model_path)
+    evaluate(model=None , inp_images= im_files , annotations= ann_files, checkpoints_path=model_path,epoch = args.epoch, visualize = args.visualize, output_folder = args.output_folder)
 
 if __name__ == "__main__":
     main()
