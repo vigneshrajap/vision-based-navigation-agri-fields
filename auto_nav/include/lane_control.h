7edfe1af5d4552704d2b06755f90206b7917d8a2
Vignesh <vignesh.raja199@gmail.com>
2019-10-02 18:42:27 +0200

    posearray from camera subscribe and global posearray publish added in lane control script

--- auto_nav/include/lane_control.h
+++ auto_nav/include/lane_control.h
@@ -3,6 +3,7 @@
 #include "Eigen/Core"
 #include <Eigen/Geometry>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseArray.h>
 #include <tf/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h> /*doTransform*/
 #include "tf_conversions/tf_eigen.h" // Conversion from eigen to TF
@@ -13,18 +14,30 @@ class Lane_control{
 public:
   double position_error = 0, q_x = 0 , q_y = 0, yaw = 0, angular_velocity = 0;
   geometry_msgs::Pose thorvald_pose;
-  tf::StampedTransform cam_t;
-  tf::TransformListener cam_listener;
+  tf::StampedTransform robot_t, cam_t;
+  geometry_msgs::Twist est_twist_msgs;
+  tf::TransformListener robot_pose_listener, cam_listener;
   geometry_msgs::Point mini_goal_pts;
   std_msgs::Float64 angular_error;
   Eigen::MatrixXd K = Eigen::MatrixXd::Zero(3,3); // K matrix for camera intrinsic
+  geometry_msgs::PoseArray poses_cam, poses_world;
+  bool row_follow_mode = false;
+
+  ros::NodeHandle nh_;
+
+  // Subscribers
+  ros::Subscriber posearray_local_sub;
+
+  // Publishers
+  ros::Publisher cmd_velocities, posearray_world;
 
   Lane_control();
+  void posesCallback (const geometry_msgs::PoseArray::ConstPtr& poses_msg);
 
   void move();
   void initialize();
   double normalizeangle(double bearing);
   Eigen::Vector3d camera2world(Eigen::Vector3d& x_c, Eigen::Vector3d& t_c, Eigen::Quaternionf& R_c);
-  void controller();
+  void controller(geometry_msgs::PoseArray goal_pts);
 
 };
--- auto_nav/src/lane_control.cpp
+++ auto_nav/src/lane_control.cpp
@@ -1,10 +1,25 @@
 #include "lane_control.h"
 
-Lane_control::Lane_control(){}
+Lane_control::Lane_control(){
+
+    posearray_local_sub = nh_.subscribe("posearray_local", 100, &Lane_control::posesCallback, this);
+
+    cmd_velocities = nh_.advertise<geometry_msgs::Twist>("nav_vel", 100);  // control
+    posearray_world = nh_.advertise<geometry_msgs::PoseArray>("posearray_global", 100);  // control
+}
 
 void Lane_control::initialize(){
-
+  cam_listener.waitForTransform("map", "camera_color_optical_frame", ros::Time(), ros::Duration(1.0)); // create the listener
+  robot_pose_listener.waitForTransform("map", "base_link", ros::Time(), ros::Duration(1.0)); // create the listener
 }
+
+// PoseArray data from camera
+void Lane_control::posesCallback (const geometry_msgs::PoseArray::ConstPtr& poses_msg){
+    poses_cam.header = poses_msg->header;
+    if(poses_msg->poses.size() > 0)
+       poses_cam.poses = poses_msg->poses;
+}
+
 // Normalize the bearing
 double Lane_control::normalizeangle(double bearing){
   if (bearing < -M_PI)
@@ -30,7 +45,11 @@ Eigen::Vector3d x_p = d*x_w+t_c;
 return x_p;
 }
 
-void Lane_control::controller(){
+void Lane_control::controller(geometry_msgs::PoseArray goal_pts){
+  mini_goal_pts = goal_pts.poses[10].position;
+
+  robot_pose_listener.lookupTransform("map", "base_link", ros::Time(0), robot_t);  // Converts to World (Map) Frame
+
   // calculation of error
   q_x = mini_goal_pts.x - thorvald_pose.position.x;
   q_y = mini_goal_pts.y - thorvald_pose.position.y;
@@ -40,6 +59,11 @@ void Lane_control::controller(){
   angular_error.data = normalizeangle(atan2(q_y, q_x) - yaw);
 
   angular_velocity = normalizeangle(atan2(2*1.05*sin(angular_error.data),position_error)); // Pure Pursuit Controller
+
+  est_twist_msgs.linear.x = 0.3;  // SET TO ZERO UNTIL THE ABOVE ONES WORK
+  est_twist_msgs.angular.z = angular_velocity;
+
+  cmd_velocities.publish(est_twist_msgs);
 }
 
 void Lane_control::move()
@@ -50,23 +74,31 @@ void Lane_control::move()
   while(ros::ok()){
   ros::spinOnce();
 
-  // if(row_follow_mode == true){ // row follow mode
+  if(row_follow_mode == true){ // row follow mode
 
     cam_listener.lookupTransform("map", "camera_color_optical_frame", ros::Time(0), cam_t);  // Converts to World (Map) Frame
-
-    Eigen::Vector3d p_c; //added roi_y for fitting original image
-
-    // Eigen::Vector3d p_c(l[2*e_pts]+roi_x,l[(2*e_pts)+1]+roi_y,1); //added roi_y for fitting original image
-
-    Eigen::Vector3d x_c(K.inverse()*p_c); // Intrinsic Calibration
     Eigen::Vector3d t_c(cam_t.getOrigin().x(),cam_t.getOrigin().y(),cam_t.getOrigin().z());
     Eigen::Quaternionf R_c(cam_t.getRotation().w(),cam_t.getRotation().x(),cam_t.getRotation().y(),cam_t.getRotation().z());
-    x_c = x_c.normalized();
 
-    Eigen::Vector3d x_p = camera2world(x_c,t_c,R_c); // Intersection Point Function
-    controller(); // low-level controller
+    //Eigen::Vector3d p_c; //added roi_y for fitting original image
+    if(poses_cam.poses.size() > 0){
+      for(int i = 0; i<poses_cam.poses.size(); i++){
+        Eigen::Vector3d p_c(poses_cam.poses[i].position.x,poses_cam.poses[i].position.y,1); //added roi_y for fitting original image
+        Eigen::Vector3d x_c(K.inverse()*p_c); // Intrinsic Calibration
+        x_c = x_c.normalized();
 
-  // } // row follow mode
+        Eigen::Vector3d x_p = camera2world(x_c, t_c, R_c); // Intersection Point Function
+        geometry_msgs::Pose pf;
+        pf.position.x = x_p[0];
+        pf.position.y = x_p[1];
+        poses_world.poses.push_back(pf);
+      }
+      posearray_world.publish(poses_world);
+    }
+
+    controller(poses_world); // low-level controller
+
+   } // row follow mode
 
   r.sleep();
   }
