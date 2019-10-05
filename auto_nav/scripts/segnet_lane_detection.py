c085026af53308263ff71fc6bfff162e311ab118
Vignesh <vignesh.raja199@gmail.com>
2019-10-05 20:05:13 +0200

    segnet lane detection file works with changes: works with image topic live, publish centerline ROS topic at the end

--- auto_nav/scripts/segnet_lane_detection.py
+++ auto_nav/scripts/segnet_lane_detection.py
@@ -15,10 +15,9 @@ from cv_bridge import CvBridge
 import sys
 sys.path.insert(1, '../../../image-segmentation-keras/')
 from keras_segmentation import predict
-sys.path.insert(1, '../../')
+sys.path.insert(2, '../../')
 import lane_predict
-
-from std_msgs.msg import String
+from geometry_msgs.msg import Pose, PoseArray
 
 # #--- ROS setup
 # image_topic_name = rosparam.get_param('auto_nav/segnet_lane_detection/camera_topic')
@@ -29,19 +28,70 @@ from std_msgs.msg import String
 rospy.init_node('segnet_lane_detection')
 
 #Set up publisher for prediction messages
-#pub = rospy.Publisher('lane_pred', String, queue_size = 1) #fixme other type
-rate = rospy.Rate(10) # 10hz
+pub = rospy.Publisher('centerline_local', PoseArray, queue_size = 10)
+rate = rospy.Rate(1) # 10hz
 
-model_prefix = "/home/vignesh/Third_Paper/segnet_weights/resnet50_segnet"
-epoch = 21
-#model = predict.model_from_checkpoint_files( model_prefix, epoch)#model_config, model_weights)
-model = predict.model_from_checkpoint_path(model_prefix, epoch)
 
-def recv_image_msg(image_msg,format = "passthrough"):
+
+visualize = "lane_fit" # "all"
+
+#image = cv2.imread("/home/saga/Third_Paper/Datasets/Frogn_Fields/Frogn_004/frogn_40000.png")
+
+#seg_arr, input_image, output_image, fit = lane_predict.predict_on_image(model, image, lane_fit = True, evaluate = False, visualize = None, output_file = None, display=False)
+
+def recv_image_msg(image_msg, format = "bgr8"): #"passthrough"):
     cv_br = CvBridge()
     #rospy.loginfo('Receiving image')
     image = cv_br.imgmsg_to_cv2(image_msg,format)
     return image
+
+def visualize_segmentation(input_img, seg_arr, n_classes, display = False, output_file = None, class_number = 2):
+    seg_img = predict.segmented_image_from_prediction(seg_arr, n_classes = n_classes, input_shape = input_img.shape)
+    overlay_img = cv2.addWeighted(input_img,0.7,seg_img,0.3,0)
+
+    # Reshaping the Lanes Class into binary array and Upscaling the image as input image
+    dummy_img = np.zeros(seg_arr.shape)
+    dummy_img += ((seg_arr[:,: ] == class_number)*(255)).astype('uint8') # Class Number 2 belongs to Lanes
+    original_h, original_w = overlay_img.shape[0:2]
+    upscaled_img = cv2.resize(dummy_img, (original_w,original_h)).astype('uint8')
+    upscaled_img_rgb = cv2.cvtColor(upscaled_img, cv2.COLOR_GRAY2RGB)
+
+    # Stack input and segmentation in one video
+    vis_img = np.vstack((
+       np.hstack((input_img,
+                  seg_img)),
+       np.hstack((overlay_img,
+                  upscaled_img_rgb)) #np.ones(overlay_img.shape,dtype=np.uint8)*128))
+    ))
+
+    return vis_img
+
+def visualize_lane_fit(input_img, output_img, n_classes, display = False, output_file = None, class_number = 2):
+    # Stack input and segmentation in one video
+    vis_img = np.hstack((input_img,np.ones(input_img.shape,dtype=np.uint8)*128)) #output_img))
+
+    return vis_img
+
+def visualize_functions(visualize=None, input_image=None, seg_arr=None, output_image=None):
+    # visualize: None, "all" or one of, "segmentation", "lane_fit"
+    if visualize == "segmentation":
+        vis_img = visualize_segmentation(input_image, seg_arr, model.n_classes, display=False, output_file=None, class_number=2)
+        #cv2.imwrite('Segmentation.png', vis_img)
+
+    elif visualize == "lane_fit":
+        vis_img = visualize_lane_fit(input_image, output_image, model.n_classes, display=False, output_file=None, class_number=2)
+        #cv2.imwrite('Lane_fit.png', vis_img)
+
+    elif visualize == "all":
+        vis_img = visualize_segmentation(input_image, seg_arr, model.n_classes, display=False, output_file=None, class_number=2)
+        #cv2.imwrite('Segmentation.png', vis_img)
+        vis_img = visualize_lane_fit(input_image, output_image, model.n_classes, display=False, output_file=None, class_number=2)
+        #cv2.imwrite('Lane_fit.png', vis_img)
+
+    else:
+        vis_img = None
+
+    cv2.destroyAllWindows()
 
 def callback(image_msg):
     #Read image
@@ -52,22 +102,53 @@ def callback(image_msg):
     # Preprocess
     rospy.loginfo('Received image for prediction')
 
-    # Prediction
-    lane_predict.predict_on_image(model, inp=image, lane_fit = True, evaluate = False, visualize = "segmentation", output_file = None, display=True)
+    model_prefix = "/home/saga/Third_Paper/segnet_weights/resnet50_segnet"
+    epoch = 21
+    #model = predict.model_from_checkpoint_files( model_prefix, epoch)#model_config, model_weights)
+    model = predict.model_from_checkpoint_path(model_prefix, epoch = None)
 
-#     #Make ros pred message
-#     #fixme return lane prediction from predict
-#     #lane_msg = "hello world %s" #tmp
-#     #publish prediction
-#     rospy.loginfo(lane_msg)
-#     pub.publish(lane_msg)
-#     rate.sleep()
-#
+    #Run prediction (and optional, visualization)
+    seg_arr, input_image, output_image, fit = lane_predict.predict_on_image(model, image, lane_fit = False, evaluate = False, visualize = None, output_file = None, display=False)
+
+    # while not rospy.is_shutdown():
+    #     #print len(fit)
+    #     if fit!=None :
+    #        fit_points = PoseArray()
+    #        fit_points.header.frame_id = "camera_color_optical_frame"
+    #        fit_points.header.stamp = rospy.Time.now()
+    #        for i in range(0,len(fit),4): # Increment by 4
+    #            P = Pose()
+    #            P.position.x = fit[i][0]
+    #            P.position.y = fit[i][1]
+    #            fit_points.poses.append(P)
+    #        pub.publish(fit_points)
+    #        rate.sleep()
+    #
+    # final_msg = visualize_functions(visualize, image, seg_arr=None, output_image=None)
+    # cv2.startWindowThread()
+    # cv2.namedWindow('preview', cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow('preview', 800,800)
+    # cv2.imshow('preview', final_msg)
+    #
+    # # Press Q on keyboard to  exit
+    # if cv2.waitKey(25) & 0xFF == ord('q'):
+    #    print('Q pressed, breaking')
+       #break
+    #Make ros pred message
+    #fixme return lane prediction from predict
+    #lane_msg = "hello world %s" #tmp
+    #publish prediction
+    #rospy.loginfo(lane_msg)
+    #pub.publish(lane_msg)
+    #rate.sleep()
+
+
+
 def predict_lane():
     #Listen to image messages and publish predictions with callback
-    rospy.Subscriber("/camera/color/image_raw", Image, callback, queue_size = 1) #rename topic in launch? #image_topic_name
+    rospy.Subscriber("/camera/color/image_raw", Image, callback, queue_size = 10) #rename topic in launch? #image_topic_name
+
     rospy.spin()
-    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     try:
--- lane_predict.py
+++ lane_predict.py
@@ -14,6 +14,7 @@ import sys
 sys.path.insert(1, '../image-segmentation-keras')
 from keras_segmentation import predict
 import sliding_window_approach
+from geometry_msgs.msg import Pose, PoseArray
 
 def upscaling_warping_parameters(rgb_img, seg_arr, class_number, crop_ratio):
    # Reshaping the Lanes Class into binary array and Upscaling the image as input image
@@ -41,7 +42,7 @@ def lane_fit_on_prediction(Roi_img, src, dst, dst_size):
    out_img, curves, lanes, ploty = sliding_window_approach.sliding_window(warped_img, modifiedCenters)
    return warped_img, out_img, curves, lanes, ploty, modifiedCenters
 
-def visualize_lane_fit_1(input_image, out_img, curves, lanes, ploty, modifiedCenters, src, dst, dst_size, crop_ratio):
+def visualize_lane_fit(input_image, out_img, curves, lanes, ploty, modifiedCenters, src, dst, dst_size, crop_ratio):
    # Visualize the fitted polygonals (One on each lane and on average curve)
    out_img, midLane_i = sliding_window_approach.visualization_polyfit(out_img, curves, lanes, ploty, modifiedCenters)
 
@@ -49,12 +50,14 @@ def visualize_lane_fit_1(input_image, out_img, curves, lanes, ploty, modifiedCen
    invwarp, Minv = sliding_window_approach.inv_perspective_warp(out_img, (dst_size[1], dst_size[0]), dst, src)
 
    midPoints = []
+   #midPoints = PoseArray()
    for i in midLane_i:
      point_wp = np.array([i[0],i[1],1])
      midLane_io = np.matmul(Minv, point_wp) # inverse-M*warp_pt
      midLane_n = np.array([midLane_io[0]/midLane_io[2],midLane_io[1]/midLane_io[2]]) # divide by Z point
      midLane_n = midLane_n.astype(int)
      midPoints.append(midLane_n)
+     #midPoints.poses.append(Pose((midLane_n[0],midLane_n[1],0),(0,0,0,1)))
 
    # Combine the result with the original image
    final_img = input_image.copy()
@@ -63,10 +66,9 @@ def visualize_lane_fit_1(input_image, out_img, curves, lanes, ploty, modifiedCen
                                                            1, invwarp, 0.9, 0)
    return out_img, midPoints, invwarp, final_img
 
-def visualize_lane_fit(input_image, seg_arr):
-
-   class_number = 2 ### Extract Interesting Class (2 - Lanes in this case) from predictions
-   crop_ratio = 0.3 ### Ratio to crop the background parts in the image from top
+def run_lane_fit(input_image, seg_arr, class_number = 2,  crop_ratio = 0.3):
+# Extract Interesting Class (2 - Lanes in this case) from predictions
+# Ratio to crop the background parts in the image from top
 
    # Setting the parameters for upscaling and warping-unwarping
    Roi_img, src, dst, dst_size = upscaling_warping_parameters(input_image, seg_arr, class_number, crop_ratio)
@@ -75,14 +77,14 @@ def visualize_lane_fit(input_image, seg_arr):
    warp_img, out_img, curves, lanes, ploty, modifiedCenters = lane_fit_on_prediction(Roi_img, src, dst, dst_size)
 
    # Overlay the inverse warped image on input image
-   polyfit_img, centerLine, invwarp, final_img = visualize_lane_fit_1(input_image, out_img, curves, lanes, ploty, modifiedCenters, src, dst, dst_size, crop_ratio)
+   polyfit_img, centerLine, invwarp, final_img = visualize_lane_fit(input_image, out_img, curves, lanes, ploty, modifiedCenters, src, dst, dst_size, crop_ratio)
    return final_img, centerLine
 
 def visualize_segmentation(input_img, seg_arr, n_classes,display = False, output_file = None):
     seg_img = predict.segmented_image_from_prediction(seg_arr, n_classes = n_classes, input_shape = input_img.shape)
     overlay_img = cv2.addWeighted(input_img,0.7,seg_img,0.3,0)
-    # Stack input and segmentation in one video
 
+    # Stack input and segmentation in one video
     vis_img = np.vstack((
        np.hstack((input_img,
                   seg_img)),
@@ -99,139 +101,67 @@ def visualization(input_img, seg_arr=None, lane_fit = None, evaluation = None, n
     if visualize == "segmentation":
         vis_img = visualize_segmentation(input_img, seg_arr, n_classes, display=display, output_file=output_file)
     if visualize == "lane_fit":
-        vis_img, centerLine = visualize_lane_fit(input_img, seg_arr)
+        vis_img, centerLine = run_lane_fit(input_img, seg_arr, class_number, crop_ratio)
         return vis_img, centerLine ##fixme
 
     if display:
         cv2.imshow('Prediction', vis_img)
     if not output_file is None:
-        cv2.imwrite(  output_file , vis_img )
+        cv2.imwrite(output_file, vis_img )
     return vis_img
 
 def predict_on_image(model, inp, lane_fit = False, evaluate = False, visualize = None, output_file = None, display=False):
+
     #visualize: None, "all" or one of, "segmentation", "lane_fit"
 
     #Run prediction (and optional, visualization)
     seg_arr, input_image = predict.predict_fast(model, inp)
 
     if lane_fit:
-        #fixme: sliding window approach lane_fit = ...
-        fit = None
+        class_number = 2 # Extract Interesting Class (2 - Lanes in this case) from predictions
+        crop_ratio = 0.3 # Ratio to crop the background parts in the image from top
+        out_img, fit = run_lane_fit(input_image, seg_arr, class_number, crop_ratio)
     else:
         fit = None
+        out_img = None
 
-    if evaluate:
-        #fixme iou,gt = evaluate(...)
-        evaluation = None
-    else:
-        evaluation = None
+    # if evaluate:
+    #     #fixme iou,gt = evaluate(...)
+    #     evaluation = None
+    # else:
+    #     evaluation = None
+    #
+    # if visualize == "segmentation":
+    #     vis_img = visualization(input_image, seg_arr=seg_arr, lane_fit=fit, evaluation=evaluation, n_classes = model.n_classes, visualize = visualize,output_file=output_file, display=display)
+    #
+    return seg_arr, input_image, out_img, fit #evaluation,
 
-    if visualize == "segmentation":
-        vis_img = visualization(input_image, seg_arr=seg_arr, lane_fit=fit, evaluation=evaluation, n_classes = model.n_classes, visualize = visualize,output_file=output_file, display=display)
-
-    return seg_arr, evaluation, vis_img, fit
-
-def predict_on_video(model, input_video_file = None, visualize = False, output_video_file = None, display = False, frame_step = 1):
-    #Initialize video capture
-    cap = cv2.VideoCapture(input_video_file)
-    # Check if video opened successfully
-    if (cap.isOpened()== False):
-        print("Error opening video stream or file")
-
-    #Initialize video writer
-    if output_video_file:
-        fourcc = cv2.VideoWriter_fourcc(*"MJPG") # ('I','Y','U','V') #tried('M','J','P','G')
-        wr = None
-        (out_h, out_w) = (None, None)
-        isColor = True
-        fps = 30
-        print('Will write to output video file : ', output_video_file)
-
-    frame_count = 0
-    frame_step = int(frame_step)
-    # Read until video is completed
-    while(cap.isOpened()):
-
-      # Capture frame-by-frame
-        ret, rgb_img = cap.read()
-        if ret == True:
-           print('Frame ',frame_count)
-           #Run prediction on video frame
-           seg_arr, input_image = predict.predict_fast(model,rgb_img)
-           seg_img = predict.segmented_image_from_prediction(seg_arr, n_classes = model.n_classes, input_shape = rgb_img.shape)
-
-           if visualize == "segmentation":
-               vis_img = visualization(input_image, seg_arr=seg_arr, lane_fit=None, evaluation=None, n_classes = model.n_classes, visualize = visualize, output_file=None, display=True)
-           if visualize == "lane_fit":
-               vis_img, midPoints = visualization(input_image, seg_arr=seg_arr, lane_fit=None, evaluation=None, n_classes = model.n_classes, visualize = visualize, output_file=None, display=True)
-
-               #Write video
-               # if output_video_file:
-               #     if wr is None: #if writer is not set up yet
-               #         (out_h,out_w) = vis_img.shape[:2]
-               #         wr = cv2.VideoWriter(output_video_file,fourcc,fps,(out_w,out_h),isColor)
-               #         wr.write(vis_img)
-
-               #Display on screen
-               if display:
-                   cv2.startWindowThread()
-                   cv2.namedWindow('preview', cv2.WINDOW_NORMAL)
-                   cv2.resizeWindow('preview', 800,800)
-                   cv2.imshow('preview', vis_img)
-
-               # Press Q on keyboard to  exit
-               if cv2.waitKey(25) & 0xFF == ord('q'):
-                   print('Q pressed, breaking')
-                   break
-           #Skip frames
-           frame_count +=frame_step
-           cap.set(1, frame_count)
-        else:
-           break
-
-    # cleanup
-    cv2.destroyAllWindows()
-    cap.release()
-    if output_video_file: wr.release()
-    return vis_img
-
-def main():
-    image_fetch = 0 ### (0 for IMAGE, 1 for VIDEO, 2 from live IMAGE TOPIC)
-
-    parser = argparse.ArgumentParser(description="Example: Run prediction on an image folder. Example usage: python lane_predict.py --model_prefix=models/resnet_3class --epoch=25 --input_folder=Frogn_Dataset/images_prepped_test --output_folder=.")
-    parser.add_argument("--model_prefix", default = '', help = "Prefix of model filename")
-    parser.add_argument("--epoch", default = None, help = "Checkpoint epoch number")
-    parser.add_argument("--display",default = False, help = "Whether to display video on screen (can be slow)")
-
-    if image_fetch == 0:
-     parser.add_argument("--input_folder",default = '', help = "(Relative) path to input image file")
-     parser.add_argument("--output_folder", default = '', help = "(Relative) path to output image file. If empty, image is not written.")
-
-    if image_fetch == 1:
-     parser.add_argument("--input_video_file",default = '', help = "(Relative) path to input video file")
-     parser.add_argument("--output_video_file", default = '', help = "(Relative) path to output video file.")
-
-    args = parser.parse_args()
-
-    #Load model
-    model = predict.model_from_checkpoint_path(args.model_prefix, args.epoch)
-
-    if image_fetch == 0:
-        print('Output_folder',args.output_folder)
-        im_files = glob.glob(os.path.join(args.input_folder,'*.png'))
-        print(os.path.join(args.input_folder+'*.png'))
-        for im in im_files:
-            if args.output_folder:
-                output_file = os.path.join(args.output_folder,os.path.basename(im)+"_lane_pred.png")
-                print(output_file)
-            else:
-                output_file = None
-            predict_on_image(model, inp = im, lane_fit = False, evaluate = False, visualize = "lane_fit", output_file = output_file, display=True)
-
-    if image_fetch == 1:
-         predict_on_video(model, input_video_file = args.input_video_file, visualize = "lane_fit", output_video_file = args.output_video_file, display = True, frame_step = 5)
-
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+# def main():
+#
+#     parser = argparse.ArgumentParser(description="Example: Run prediction on an image folder. Example usage: python lane_predict.py --model_prefix=models/resnet_3class --epoch=25 --input_folder=Frogn_Dataset/images_prepped_test --output_folder=.")
+#     parser.add_argument("--model_prefix", default = '', help = "Prefix of model filename")
+#     parser.add_argument("--epoch", default = None, help = "Checkpoint epoch number")
+#     parser.add_argument("--display",default = False, help = "Whether to display video on screen (can be slow)")
+#     parser.add_argument("--input_folder",default = '', help = "(Relative) path to input image file")
+#     parser.add_argument("--output_folder", default = '', help = "(Relative) path to output image file. If empty, image is not written.")
+#
+#     args = parser.parse_args()
+#
+#     #Load model
+#     model = predict.model_from_checkpoint_path(args.model_prefix, args.epoch)
+#
+#     print('Output_folder',args.output_folder)
+#     im_files = glob.glob(os.path.join(args.input_folder,'*.png'))
+#     print(os.path.join(args.input_folder+'*.png'))
+#     for im in im_files:
+#         if args.output_folder:
+#             output_file = os.path.join(args.output_folder,os.path.basename(im)+"_lane_pred.png")
+#             print(output_file)
+#         else:
+#             output_file = None
+#         predict_on_image(model, inp = im, lane_fit = False, evaluate = False, visualize = "segmentation", output_file = output_file, display=True)
+#
+#     cv2.destroyAllWindows()
+#
+# if __name__ == "__main__":
+#     main()
