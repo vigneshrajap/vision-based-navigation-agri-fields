159f5315fb79c7b2c1926089e3e8027c7e6ceb80
Vignesh <vignesh.raja199@gmail.com>
2019-10-09 20:36:13 +0200

    removed realsense libraries, labelme

--- auto_nav/scripts/segnet_lane_detection.py
+++ auto_nav/scripts/segnet_lane_detection.py
@@ -49,11 +49,11 @@ class lane_finder():
         self.fit = []
         self.class_number = 2
 
-        self.loaded_weights = False
+        #self.loaded_weights = False
         self.img_receive = False
         self.epoch = None
         #model = predict.model_from_checkpoint_files( model_prefix, epoch)#model_config, model_weights)
-        self.model, self.loaded_weights = predict.model_from_checkpoint_path(self.model_prefix, self.epoch)
+        self.model = predict.model_from_checkpoint_path(self.model_prefix, self.epoch) #, self.loaded_weights
 
         #Listen to image messages and publish predictions with callback
         self.img_sub = rospy.Subscriber(self.image_topic_name, Image, self.imageCallback)
@@ -157,7 +157,7 @@ class lane_finder():
 
     def pipeline(self):
 
-      if self.img_receive and self.loaded_weights:
+      if self.img_receive: #and self.loaded_weights
         #Run prediction (and optional, visualization)
         self.seg_arr,self.image,self.output_image,self.fit = lane_predict.predict_on_image(self.model,self.image,self.lane_fit,self.evaluate,self.visualize,self.output_file,self.display)
 
