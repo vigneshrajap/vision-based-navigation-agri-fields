185e42eac4db3a5a021dff2b0dbe9bd7aa6efb83
Marianne <marianne.bakken@sintef.no>
2020-01-14 12:52:33 +0100

    Working version with function calls and example script.

--- auto_label/field_mask.py
+++ auto_label/field_mask.py
@@ -13,6 +13,65 @@ import os
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
+#%% General transformation stuff
+'''
+Roll, pitch, yaw is rotation around x,y,z axis. 
+To combine rotation matrices, rotate in x,y,z order around x axis first: R=RzRyRx
+
+'''
+
+def x_rotation_matrix(theta):
+    Rx = np.array([
+            np.array([1, 0, 0]),
+            np.array([0, np.cos(theta), np.sin(theta)]),
+            np.array([0, -np.sin(theta), np.cos(theta)])
+            ])
+    return Rx
+
+def y_rotation_matrix(theta):
+    Ry = np.array([
+            [np.cos(theta), 0, -np.sin(theta)],
+            [0, 1, 0],
+            [np.sin(theta), 0, np.cos(theta)]
+            ])
+    return Ry
+    
+def z_rotation_matrix(theta):
+    Rz = np.array([
+            [np.cos(theta), np.sin(theta), 0],
+            [-np.sin(theta), np.cos(theta), 0],
+            [0, 0, 1]
+            ])
+    return Rz
+
+def create_transformation_matrix(r,t):
+    '''
+    Make a homogeneous 4x4 translation matrix from rotation angles (in radians) rx,ry,rz and translations (in meteres) tx,ty,tz
+    Transformation order:
+        1. x axis rotation
+        2. y axis rotation
+        3. z axis rotation
+        4. translation
+    '''
+    rx,ry,rz = r 
+    tx,ty,tz = t
+    Rx = x_rotation_matrix(rx)
+    Ry = y_rotation_matrix(ry)
+    Rz = z_rotation_matrix(rz)
+    R = Rz.dot(Ry).dot(Rx) #combine rotation matrices: x rotation first
+    t = np.array([tx,ty,tz])
+    T = np.eye(4)
+    T[0:3,0:3] = R
+    T[0:3,3] = t
+    return T
+
+def transform_xyz_point(T,point):
+    P = np.eye(4)
+    P[0:3,3] = point
+    P_transformed = T.dot(P)
+    return P_transformed[0:3,3]
+    
+#%% Utilities
 def line_XY_intersection(point, direction):
     """
     Finds intersection (x,y) between XY plane and a line.
@@ -76,7 +135,13 @@ class Polygon():
                 break
         return is_inside
     
-               
+#%% Core functionality
+'''
+Assumptions:
+    Robot coordinate system: Aligned with robot base, moving with the robot. X ahead, Y to the left, Z up
+    World coordinate system: The local ground truth coordinate system. 
+    Camera coordinate system: X right, Y down, Z into the image
+'''
 
 def make_field_mask(widths,labels,extent):
     #Create adjacent rectangles with row/crop labels and save as polygons
@@ -99,27 +164,14 @@ def make_field_mask(widths,labels,extent):
         position = position + np.array([0,w]) #position of next rectangle
     
     return list_of_polygons
+
+def camera_to_world_transform(T_camera_to_robot = np.eye(4), T_robot_to_world = np.eye(4)):
+    return T_robot_to_world.dot(T_camera_to_robot)
+
+def set_up_robot_to_world_transform(rpy, xyz):
+    return create_transformation_matrix(r=rpy, t=xyz)
         
-
-#%% Transformation related stuff (move later?)
-'''
-Roll, pitch, yaw is rotation around x,y,z axis. 
-To combine rotation matrices, use Euler convension and rotate in x,y,z order around x axis first: R=RzRyRx
-
-'''
-#camera-robot specific
-
-def transform_point_camera_to_world(point = [0,0,0], T_camera_to_robot = np.eye(4), T_robot_to_world = np.eye(4)):
-    '''
-    Transform point from camera coordinates to world coordinates. 
-    '''
-    T_cam_to_world = T_robot_to_world.dot(T_camera_to_robot)
-    return transform_xyz_point(T_cam_to_world,point)
-
-def set_up_robot_to_world_transform(robot_rpy, robot_xyz):
-    return create_transformation_matrix(r=robot_rpy, t=robot_xyz)
-        
-def set_up_camera_to_robot_transform(camera_rpy = [0,0,0],camera_xyz = [0,0,0]):
+def set_up_camera_to_robot_transform(rpy = [0,0,0], xyz = [0,0,0]):
     ''' 
     Robot coordinates: x ahead, y left, z up
     Camera coordinaes: x right, y down, z ahead
@@ -132,129 +184,79 @@ def set_up_camera_to_robot_transform(camera_rpy = [0,0,0],camera_xyz = [0,0,0]):
     ry = 0
     rz = np.pi/2
     T_cam_to_camaligned = create_transformation_matrix(r = [rx,ry,rz],t = [0,0,0])
-    
-    #camera tilt and position compared to robot coordinate system
-    T_camaligned_to_rob = create_transformation_matrix(r = camera_rpy,t = camera_xyz)
-    
+    #Camera tilt and position compared to robot coordinate system
+    T_camaligned_to_rob = create_transformation_matrix(r = rpy,t = xyz)
+    #Combine 
     T_cam_to_rob = T_camaligned_to_rob.dot(T_cam_to_camaligned)
     return T_cam_to_rob
 
-
-#General
-#Rotation matrices independent of coordinate system definition: 
-def x_rotation_matrix(theta):
-    Rx = np.array([
-            np.array([1, 0, 0]),
-            np.array([0, np.cos(theta), np.sin(theta)]),
-            np.array([0, -np.sin(theta), np.cos(theta)])
-            ])
-    return Rx
-
-def y_rotation_matrix(theta):
-    Ry = np.array([
-            [np.cos(theta), 0, -np.sin(theta)],
-            [0, 1, 0],
-            [np.sin(theta), 0, np.cos(theta)]
-            ])
-    return Ry
-    
-def z_rotation_matrix(theta):
-    Rz = np.array([
-            [np.cos(theta), np.sin(theta), 0],
-            [-np.sin(theta), np.cos(theta), 0],
-            [0, 0, 1]
-            ])
-    return Rz
-
-def create_transformation_matrix(r,t):
+def make_image_mask_from_polygons(cam_model,polygon_mask,T_cam_to_world,cropped_dims = None):
+    ''' 
+    Make an image mask from a polygon_mask in world coordinates, based on camera model and camera to world transform
     '''
-    Make a homogeneous 4x4 translation matrix from rotation angles (in radians) rx,ry,rz and translations (in meteres) tx,ty,tz
-    Transformation order:
-        1. x axis rotation
-        2. y axis rotation
-        3. z axis rotation
-        4. translation
-    '''
-    rx,ry,rz = r 
-    tx,ty,tz = t
-    Rx = x_rotation_matrix(rx)
-    Ry = y_rotation_matrix(ry)
-    Rz = z_rotation_matrix(rz)
-    R = Rz.dot(Ry).dot(Rx) #combine rotation matrices: x rotation first
-    t = np.array([tx,ty,tz])
-    T = np.eye(4)
-    T[0:3,0:3] = R
-    T[0:3,3] = t
-    return T
+    if cropped_dims is None:
+        cropped_dims = [cam_model.height,cam_model.width]
+        crop_h_start = 0
+        crop_w_start = 0
+    else:
+        crop_h_start = np.round(cam_model.height/2-cropped_dims[0]/2)
+        crop_w_start = np.round(cam_model.width/2-cropped_dims[1]/2)
 
-def transform_xyz_point(T,point):
-    P = np.eye(4)
-    P[0:3,3] = point
-    P_transformed = T.dot(P)
-    return P_transformed[0:3,3]
+    camera_origo = transform_xyz_point(T_cam_to_world,[0,0,0]) #camera origo in world coordinates
     
-
-if __name__ == "__main__":
-    #%% Make a field mask
-    list_of_polygons = make_field_mask(widths = [0.5,0.3,0.5], labels = [1,0,1], extent = 5)#position = [row_offset-row_width/2,0])
-    for p in list_of_polygons:
-        print(p.points)
-    box1 = list_of_polygons[0]
-    #is_inside = box1.check_if_inside([0.45,1])
-    #print(is_inside)
-    
-    #%%Transformations 
-    
-    #Transform pixel
-    calib_file = os.path.join('../auto_nav/scripts/input_cam_model_campus_2018-08-31.xml')
-    cam_model = OcamCalibCameraModel(calib_file)
-    vector = cam_model.pixel_to_vector(cam_model.width*1/2, cam_model.height*1/2)    
-    normalized_vector = vec3_normalise(vector)
-    print(vector,normalized_vector)
-    
-    #Camera robot transformations
-    T_camera_to_robot = set_up_camera_to_robot_transform(camera_rpy = [0,-np.pi/8,0],camera_xyz =[0,0,1])
-    vector_robot = transform_xyz_point(T_camera_to_robot,vector)
-    print('vector_robot', vector_robot)
-    
-    T_robot_to_world = set_up_robot_to_world_transform(robot_rpy = [0,0,0],robot_xyz = [0,0,0])
-    vector_world = transform_point_camera_to_world(point = vector,T_camera_to_robot = T_camera_to_robot, T_robot_to_world = T_robot_to_world)
-    print('vector_world',vector_world)
-    
-    # Check if inside any polygons
-    camera_origo = transform_point_camera_to_world(point=[0,0,0],T_camera_to_robot=T_camera_to_robot, T_robot_to_world=T_robot_to_world)
-    print('camera_origo',camera_origo)
-    ground_point = line_XY_intersection(point=camera_origo,direction = np.array(vector_world)-np.array(camera_origo))
-    print('ground_point', ground_point)
-    point_is_inside = box1.check_if_inside(ground_point)
-    print('Point',ground_point, 'is inside box ', box1.points, ': ', point_is_inside)
-    
-    # For a set of pixels
-    
-    cropped_dims = [200,300]
-    h_start = np.round(cam_model.height/2-cropped_dims[0]/2)
-    h_stop = np.round(cam_model.height/2+cropped_dims[0]/2)
-    w_start = np.round(cam_model.width/2-cropped_dims[1]/2)
-    w_stop = np.round(cam_model.width/2+cropped_dims[1]/2)
-    
+    #Generate mask image
     mask_image = np.zeros((cropped_dims[0],cropped_dims[1],2))
-    #for j in tqdm(np.arange(mask_image.shape[1])):
     for j in tqdm(np.arange(mask_image.shape[1])):
         for i in np.arange(mask_image.shape[0]):
-            pixel = [i+h_start,j+w_start]
-            v = cam_model.pixel_to_vector(pixel[1],pixel[0])
-            v_world = transform_point_camera_to_world(point = v,T_camera_to_robot = T_camera_to_robot, T_robot_to_world = T_robot_to_world)
-            gp = line_XY_intersection(point=camera_origo,direction = np.array(v_world)-np.array(camera_origo))
-            for poly_index,poly in enumerate(list_of_polygons):
-                if poly.check_if_inside(gp):
-                    mask_image[i,j,0] = poly_index
-                    mask_image[i,j,1] = poly.label
-                    break
+            pixel = [i+crop_h_start,j+crop_w_start]
+            mask_image[i,j,0],mask_image[i,j,1] = check_if_pixel_inside_mask(polygon_mask,cam_model,pixel,T_cam_to_world,camera_origo)
+    return mask_image
     
+def check_if_pixel_inside_mask(polygon_mask,cam_model,pixel_yx, T_cam_to_world,camera_origo):
+    '''
+    For a given camera model, check if pixel y,x is inside 
+    '''
+    v = cam_model.pixel_to_vector(pixel_yx[1],pixel_yx[0])
+    v_world = transform_xyz_point(T_cam_to_world,v)
+    gp = line_XY_intersection(point=camera_origo,direction = np.array(v_world)-np.array(camera_origo))
+    for poly_index,poly in enumerate(polygon_mask):
+        if poly.check_if_inside(gp):
+            #mask_image[i,j,0] = poly_index
+            #mask_image[i,j,1] = poly.label
+            return poly_index, poly.label
+            break
+    return np.NaN, np.NaN
+
+if __name__ == "__main__":
+    #%% Demo code: Hot to make an image field mask
+    
+    #Dummy field mask
+    polygon_field_mask = make_field_mask(widths = [0.5,0.3,0.5], labels = [1,0,1], extent = 5)
+    
+    #%%Transformations 
+    #Old camera model with adjustments for testing
+    calib_file = os.path.join('../auto_nav/scripts/input_cam_model_campus_2018-08-31.xml')
+    cam_model = OcamCalibCameraModel(calib_file)
+
+    #Camera setup
+    #dummy values, should get values from outside
+    camera_tilt = np.pi/8
+    camera_height = 1
+    T_camera_to_robot = set_up_camera_to_robot_transform(rpy = [0,-camera_tilt,0], xyz =[0,0,camera_height])    
+    
+    #Robot position 
+    #dummy values, should get values from outside
+    robot_rpy = [0,0,0]
+    robot_xyz = [0,0,0]
+    T_robot_to_world = set_up_robot_to_world_transform(rpy = robot_rpy, xyz = robot_xyz)
+    
+    T_cam_to_world = camera_to_world_transform(T_camera_to_robot, T_robot_to_world)
+    image_mask = make_image_mask_from_polygons(cam_model, polygon_field_mask, T_cam_to_world, cropped_dims = [200,300])
+        
     plt.figure(10)
-    plt.imshow(mask_image[:,:,0])  
+    plt.imshow(image_mask[:,:,0])  
     plt.figure(11)          
-    plt.imshow(mask_image[:,:,1])
-    
+    plt.imshow(image_mask[:,:,1])
+
         
             
