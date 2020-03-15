16786205ed33eb26f7a121c0fb9734b43a357d16
Vignesh <vignesh.raja199@gmail.com>
2020-03-16 00:00:23 +0100

    offset estimation using rosbag API

--- auto_nav/scripts/lateral_heading_offset_from_rosbag.py
+++ auto_nav/scripts/lateral_heading_offset_from_rosbag.py
@@ -20,6 +20,11 @@ import shapely.geometry as geom
 import rospkg
 from std_msgs.msg import Header
 import csv
+import rosbag
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" # Hides the pygame version, welcome msg
+from os.path import expanduser
+import os.path as osp
 
 class automated_labelling():
     '''
@@ -28,11 +33,12 @@ class automated_labelling():
     def __init__(self):
         self.datum = [-6614855.745, -594362.895, 0.0] # Manual Datum (NEGATE if UTM is the child frame)
         self.gps_robot = [0.425, -0.62, 1.05] # Fixed Static Transform
+        self.imu_robot = [0.310, 0.00, 0.80] # Fixed Static Transform
 
         rospack = rospkg.RosPack()
         self.book = pe.get_book(file_name=rospack.get_path('auto_nav')+"/config/ground_truth_coordinates.xls", start_row=1)
 
-        self.lane_number = str(1) #rospy.set_param('lane_number', 1)
+        self.lane_number = str(4) #rospy.set_param('lane_number', 1)
         self.gt_utm = np.empty([self.book["Sheet"+self.lane_number].number_of_rows(), 2])
         self.gt_map = np.empty([self.book["Sheet"+self.lane_number].number_of_rows(), 2])
 
@@ -76,12 +82,13 @@ class automated_labelling():
         self.img_NewTime = []
         self.img_oldSeq = []
         self.img_NewSeq = []
+
+        self.oneshot_gps = 0
+        self.oneshot_imu = 0
+        self.oneshot_img = 0
         self.dt_gps = 0
         self.dt_imu = 0
         self.dt_img = 0
-        self.dt_imgSeq = 0
-        self.oneshot = 0
-        self.oneshot_img=0
 
         self.listener = tf.TransformListener()
         self.map_trans = TransformStamped()
@@ -91,59 +98,12 @@ class automated_labelling():
         self.map_trans.transform.translation = Vector3(self.datum[0], self.datum[1], self.datum[2]);
         self.map_trans.transform.rotation = Quaternion(0,0,0,1) # Set to identity
 
-        # Subscribers Callback
-        self.fix_sub = rospy.Subscriber(self.gps_topic_name, NavSatFix, self.gpsFixCallback)
-        self.imu_sub = rospy.Subscriber(self.imu_topic_name, Imu, self.imuDataCallback)
-        self.image_sub = rospy.Subscriber(self.image_topic_name, Image, self.imageCallback)
-
-    def gpsFixCallback(self, fix_msg):
-        # Read fix messages
-        self.gps_fix = fix_msg
-        self.receive_gps_fix = True
-        if (self.oneshot==0): # Set the Start time
-          self.gps_oldTime = self.gps_fix.header.stamp.to_sec()
-          self.oneshot = 1
-
-    def imuDataCallback(self, imu_msg):
-        # Read fix messages
-        self.imu_data = imu_msg
-        self.orientation_imu = [self.imu_data.orientation.x, self.imu_data.orientation.y, self.imu_data.orientation.z, self.imu_data.orientation.w]
-        (roll_imu, pitch_imu, self.yaw_imu) = euler_from_quaternion(self.orientation_imu)
-        self.receive_imu_fix = True
-
-    def recv_image_msg(self, ros_data): #"passthrough"):
-        try:
-            self.image = self.bridge.imgmsg_to_cv2(ros_data,"bgr8")
-            self.img_timestamp = ros_data.header
-            if (self.oneshot_img==0): # Set the Start time
-              self.img_oldTime = self.img_timestamp.stamp.to_sec()
-              self.img_oldSeq = self.img_timestamp.seq
-              self.oneshot_img = 1
-        except CvBridgeError as e:
-          print(e)
-
-    def imageCallback(self, ros_data):
-        # Read image
-        self.recv_image_msg(ros_data)
-        if(np.ndim(self.image) !=3 or np.shape(self.image)[2] !=3):
-            rospy.logerr('Input image must have 3 dimensions with 3 color channels')
-
-        self.img_receive = True
-
-    def qv_mult(self):
-        # rotate vector v1 by quaternion q1
-        q1 = self.orientation_imu
-        q2 = list([self.gps_robot[0],self.gps_robot[1],self.gps_robot[2],0.0])
-
-        return quaternion_multiply(
-            quaternion_multiply(q1, q2), quaternion_conjugate(q1))
-
-    def normalizeangle(self, bearing):
-      if (bearing < -math.pi):
-              bearing += 2 * math.pi
-      elif (bearing > math.pi):
-              bearing -= 2 * math.pi
-      return bearing
+        self.imu_trans = TransformStamped()
+        self.imu_trans.header.stamp = rospy.Time.now()
+        self.imu_trans.header.frame_id = self.robot_frame
+        self.imu_trans.child_frame_id = self.imu_frame
+        self.imu_trans.transform.translation = Vector3(self.imu_robot[0], self.imu_robot[1], self.imu_robot[2]);
+        self.imu_trans.transform.rotation = Quaternion(0,0,0,1) # Set to identity
 
     def ground_truth_utm2map(self):
 
@@ -158,6 +118,21 @@ class automated_labelling():
           pose_trans = tf2_geometry_msgs.do_transform_pose(pose_stamped, self.map_trans) # Transform RTK values w.r.t to "Map" frame
 
           self.gt_map[i] = ([pose_trans.pose.position.x, pose_trans.pose.position.y])
+
+    def qv_mult(self):
+        # rotate vector v1 by quaternion q1
+        q1 = self.orientation_imu
+        q2 = list([self.gps_robot[0],self.gps_robot[1],self.gps_robot[2],0.0])
+
+        return quaternion_multiply(
+            quaternion_multiply(q1, q2), quaternion_conjugate(q1))
+
+    def normalizeangle(self, bearing):
+        if (bearing < -math.pi):
+                  bearing += 2 * math.pi
+        elif (bearing > math.pi):
+                  bearing -= 2 * math.pi
+        return bearing
 
     def GNSS_WorldToRobot(self):
 
@@ -212,85 +187,145 @@ class automated_labelling():
        if ((bX - aX)*(cY - aY) - (bY - aY)*(cX - aX)) > 0:
            self.lateral_offset = -self.lateral_offset
 
-       print "lateral_offset:", self.lateral_offset
+       # print "lateral_offset:", self.lateral_offset
 
-       radius_of_curvature = multilines[0][segment_index[0][0]].length # Total Length of the line segment (min lateral offset)
-       self.gt_yaw = 1/radius_of_curvature
+       # radius_of_curvature = multilines[0][segment_index[0][0]].length # Total Length of the line segment (min lateral offset)
+       # print segment_index, aX, aY, bX, bY, math.atan2(bY-aY,bX-aX)
+
+       self.gt_yaw = self.normalizeangle(math.atan2(bX-aX,bY-aY)) #1/radius_of_curvature
 
        # Angular Offset => IMU with GT yaw (line segement index)
        if self.receive_imu_fix==True:
            self.angular_offset = self.normalizeangle(self.gt_yaw - self.yaw_imu)
-           print "angular_offset:", self.angular_offset
+           # print "angular_offset:", self.angular_offset
 
        # RGB Image nearest to current GNSS fix msg
        if self.img_receive==True:
            rospy.loginfo('Received image for prediction')
-           print
+           #print
 
 if __name__ == '__main__':
     try:
         #Initialize node
         rospy.init_node('lateral_heading_offset')
 
-        # auto_label = automated_labelling()
-        #
-        # # Function to obtain the ground truth values in Map frame
-        # auto_label.ground_truth_utm2map()
-        #
-        # myfile = open('offset_values.txt', 'a')
-        # myfile.truncate(0)
-        # myfile.write("dt(cam)")
-        # myfile.write("\t")
-        # myfile.write("frame")
-        # myfile.write("\t")
-        # myfile.write("LO")
-        # myfile.write("\t")
-        # myfile.write("AO")
-        # myfile.write("\n")
-        #
-        # while not rospy.is_shutdown():
-        #
-        #    if auto_label.receive_gps_fix== True: #auto_label.img_receive == True and
-        #
-        #        # Current Time and Relative Time "dt"
-        #        auto_label.gps_NewTime = auto_label.gps_fix.header.stamp.to_sec()
-        #        auto_label.img_NewTime = auto_label.img_timestamp.stamp.to_sec()
-        #        auto_label.img_NewSeq = auto_label.img_timestamp.seq
-        #        # auto_label.imu_NewTime = auto_label.imu_data.header.stamp.to_sec()
-        #
-        #        auto_label.dt_gps = auto_label.dt_gps + (auto_label.gps_NewTime - auto_label.gps_oldTime)
-        #        #auto_label.dt_img = auto_label.dt_img + (auto_label.img_NewTime - auto_label.img_oldTime)
-        #        #auto_label.dt_imgSeq = auto_label.img_NewSeq - auto_label.img_oldSeq #auto_label.dt_imgSeq +
-        #        # auto_label.dt_imu = auto_label.dt_imu + (auto_label.imu_NewTime - auto_label.imu_oldTime)
-        #
-        #        # print auto_label.img_timestamp.seq, auto_label.img_oldSeq, auto_label.dt_imgSeq
-        #
-        #        # RTK Fix from UTM Frame to Robot Frame
-        #        auto_label.GNSS_WorldToRobot()
-        #
-        #        # Offset Estimation
-        #        auto_label.offset_estimation()
-        #
-        #        auto_label.gps_oldTime = auto_label.gps_NewTime
-        #        auto_label.img_oldTime = auto_label.img_NewTime
-        #        # auto_label.imu_oldTime = auto_label.imu_NewTime
-        #
-        #        myfile.write(str( "%.4f" % auto_label.dt_gps))
-        #        myfile.write("\t")
-        #        #myfile.write(str( "%.4f" % auto_label.dt_gps))
-        #        #myfile.write("\t")
-        #        myfile.write(str("%04d" %auto_label.dt_imgSeq))
-        #        myfile.write("\t")
-        #        myfile.write(str("%.4f" % auto_label.lateral_offset))
-        #        myfile.write("\t")
-        #        myfile.write(str("%.4f" % auto_label.angular_offset))
-        #        myfile.write("\n")
-        #
-        #        auto_label.receive_gps_fix = False
-        #        auto_label.img_receive = False
-        #        auto_label.receive_imu_fix = False
+        auto_label = automated_labelling()
 
-               # print auto_label.dt_img  # auto_label.dt_gps, auto_label.dt_imu
+        # Function to obtain the ground truth values in Map frame
+        auto_label.ground_truth_utm2map()
+
+        myfile = open('offset_values.txt', 'a')
+        myfile.truncate(0)
+        myfile.write("dt(cam)")
+        myfile.write("\t")
+        myfile.write("frame")
+        myfile.write("\t")
+        myfile.write("LO")
+        myfile.write("\t")
+        myfile.write("AO")
+        myfile.write("\n")
+
+        input_dir = expanduser("~/Third_Paper/Datasets/20191010_L4_N/bag_files/")
+
+        for bag_file in sorted(glob.glob(osp.join(input_dir, '*.bag'))):
+            print(bag_file)
+
+            bag = rosbag.Bag(bag_file) #'/home/vignesh/Third_Paper/Datasets/20191010_L1_N/bag_files/dataset_recording_2019-10-10-14-52-14_0.bag')
+
+            # GNSS Data
+            auto_label.gps_fix_ = []
+            auto_label.dt_gps_fix_ = []
+
+            for topic, msg, t in bag.read_messages(topics=[auto_label.gps_topic_name]):
+                 if(auto_label.oneshot_gps==0):
+                     t0 = t.to_sec()
+                     auto_label.oneshot_gps = 1
+
+                 auto_label.dt_gps = auto_label.dt_gps + (t.to_sec()-t0)
+                 auto_label.dt_gps_fix_.append(auto_label.dt_gps)
+                 auto_label.gps_fix_.append([msg.latitude, msg.longitude])
+                 t0 = t.to_sec()
+            # print auto_label.dt_gps#, auto_label.dt_gps_fix_
+
+            # IMU data
+            auto_label.imu_fix_ = []
+            auto_label.dt_imu_fix_ = []
+
+            for topic, imu_msg, t_imu in bag.read_messages(topics=[auto_label.imu_topic_name]):
+                 if(auto_label.oneshot_imu==0):
+                     t0_imu = t_imu.to_sec()
+                     auto_label.oneshot_imu = 1
+                     auto_label.receive_imu_fix = True
+
+                 auto_label.dt_imu = auto_label.dt_imu + (t_imu.to_sec()-t0_imu)
+
+                 imu_data = imu_msg
+                 auto_label.orientation_imu = [imu_data.orientation.x, imu_data.orientation.y, imu_data.orientation.z, imu_data.orientation.w]
+                 (roll_imu, pitch_imu, yaw_imu) = euler_from_quaternion(auto_label.orientation_imu)
+
+                 # Transform IMU values w.r.t to "base_link" frame
+                 pose_imu = PoseStamped()
+                 pose_imu.pose.position = Point(0, 0, 0) # IMU position is meaningless, set to zero
+                 pose_imu.pose.orientation = imu_data.orientation
+
+                 # Transform IMU values w.r.t to "Map" frame
+                 pose_map_imu = PoseStamped()
+                 pose_map_imu.header.stamp = rospy.Time.now()
+                 pose_map_imu = tf2_geometry_msgs.do_transform_pose(pose_imu, auto_label.imu_trans)
+                 orientation_map_imu = [pose_map_imu.pose.orientation.x, pose_map_imu.pose.orientation.y, pose_map_imu.pose.orientation.z, pose_map_imu.pose.orientation.w]
+
+                 (roll_map_imu, pitch_map_imu, yaw_map_imu) = euler_from_quaternion(orientation_map_imu)
+                 # print yaw_imu, yaw_map_imu
+
+                 auto_label.dt_imu_fix_.append(auto_label.dt_imu)
+                 auto_label.imu_fix_.append(yaw_map_imu)
+                 t0_imu = t_imu.to_sec()
+
+            # Image data
+            auto_label.img_ = []
+            auto_label.dt_img_ = []
+            auto_label.dt_imgSeq_ = []
+
+            for topic, img_msg, t_img in bag.read_messages(topics=[auto_label.image_topic_name]):
+                 if(auto_label.oneshot_img==0):
+                     t0_img = t_img.to_sec()
+                     auto_label.img_oldSeq = img_msg.header.seq
+                     auto_label.oneshot_img = 1
+
+                 auto_label.dt_img = auto_label.dt_img + (t_img.to_sec()-t0_img)
+                 auto_label.img_NewSeq = img_msg.header.seq
+
+                 auto_label.dt_imgSeq = auto_label.img_NewSeq - auto_label.img_oldSeq
+
+                 auto_label.dt_img_.append(auto_label.dt_img)
+                 auto_label.dt_imgSeq_.append(auto_label.dt_imgSeq)
+                 t0_img = t_img.to_sec()
+
+            for i in range(len(auto_label.dt_img_)):
+
+                gps_idx = (np.abs(np.array(auto_label.dt_gps_fix_) - auto_label.dt_img_[i])).argmin()
+                auto_label.gps_fix.latitude = auto_label.gps_fix_[gps_idx][0]
+                auto_label.gps_fix.longitude = auto_label.gps_fix_[gps_idx][1]
+
+                imu_idx = (np.abs(np.array(auto_label.dt_imu_fix_) - auto_label.dt_img_[i])).argmin()
+                auto_label.yaw_imu = auto_label.imu_fix_[imu_idx]
+
+                # RTK Fix from UTM Frame to Robot Frame
+                auto_label.GNSS_WorldToRobot()
+
+                # Offset Estimation
+                auto_label.offset_estimation()
+
+                myfile.write(str( "%.4f" % auto_label.dt_img_[i]))
+                myfile.write("\t")
+                myfile.write(str("%04d" %auto_label.dt_imgSeq_[i]))
+                myfile.write("\t")
+                myfile.write(str("%.4f" % auto_label.lateral_offset))
+                myfile.write("\t")
+                myfile.write(str("%.4f" % auto_label.angular_offset))
+                myfile.write("\n")
+
+            bag.close()
 
     except rospy.ROSInterruptException:
          cv2.destroyAllWindows() # Closes all the frames
