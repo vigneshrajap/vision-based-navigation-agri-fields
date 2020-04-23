Fixed error in angle computation

auto_label/compute_robot_offsets_offline.py
@@ -26,37 +26,6 @@ def interpolate_position_to_new_time(pos_time, pos_x, pos_y, new_time):
     pos_interp_y = f_interp_y(new_time)
     return pos_interp_x, pos_interp_y
 
-'''
-def compute_offset(pos_ind, r_pos_x, r_pos_y, gt_x, gt_y, debug = False):
-    rx = r_pos_x[pos_ind]
-    ry = r_pos_y[pos_ind]
-
-    #find closest GT point
-    closest_point_ind = closest_point(rx,ry,gt_x,gt_y)
-   
-    gt_line_point, gt_line_vec = line_to_next_point(closest_point_ind, gt_x,gt_y) #naive way - point to point
-    r_point, r_vec = line_fit_from_points(pos_ind,r_pos_x,r_pos_y,forward_window = 25, backward_window = 25)
-
-    angular_offset = angle_between_vectors(gt_line_vec,r_vec)
-    lateral_offset = signed_distance_point_to_line([rx,ry],gt_line_point,gt_line_vec)
-
-    if debug:
-        if(pos_ind%50)== 0:
-            #plot upsampled vs original
-            plt.figure(1)
-            plt.plot(r_pos_x,r_pos_y,'b.-')
-            #Plot gt and robot vectors step by step
-            plt.plot(rx,ry,'r.')
-            plt.plot([rx,rx+r_vec[0]],[ry,ry+r_vec[1]],'r-')
-            plt.plot(gt_line_point[0],gt_line_point[1],'go')
-            plt.plot([gt_line_point[0],gt_line_point[0]+gt_line_vec[0]],[gt_line_point[1],gt_line_point[1]+gt_line_vec[1]],'g-')
-            plt.xlim([rx-2,rx+2])
-            plt.ylim([ry-2,ry+2])
-            plt.title('angular_offset '+  '%.3f' % angular_offset + ', lateral_offset ' + '%.3f' % lateral_offset)
-            plt.show()
-
-    return lateral_offset, angular_offset
-'''
 def compute_and_save_robot_offsets(input_dir = os.path.join('.','output'), 
     output_dir = os.path.join('.','output'), 
     rec_prefix = '20191010_L3_S_morning_slaloam', 
@@ -96,7 +65,6 @@ def compute_and_save_robot_offsets(input_dir = os.path.join('.','output'),
     #-- Compute GT-robot offsets
     for ind,im_m in enumerate(img_meta):
         try:
-            #lateral_offset, angular_offset = compute_offset(pos_ind = ind, r_pos_x = r_pos_upsampled_x, r_pos_y = r_pos_upsampled_y, gt_x = gt_x, gt_y = gt_y, debug = debug)
             gpsx = pos_upsampled_x[ind]
             gpsy = pos_upsampled_y[ind]
 
@@ -106,29 +74,18 @@ def compute_and_save_robot_offsets(input_dir = os.path.join('.','output'),
             #Transform position from GPS antenna to robot 
             r_vec = gps_vec
             yaw_angle = angle_between_vectors([1,0],r_vec)
-            #yaw_angle = -np.pi/6 #dummy rotate from GPS to map!
            
             T_gps_to_map = ht.create_transformation_matrix(r = [0,0,yaw_angle], t = [gpsx,gpsy,0])
-            #print(T_gps_to_map)
             robot_origo_in_GPS_frame = robot_to_gps_translation
-            #robot_origo_in_GPS_frame = [-2,1,0] #dummy
-            #gps_point_in_map_frame = ht.transform_xyz_point(T = T_gps_to_map,point = [1,0,0])
             r_pos = ht.transform_xyz_point(T = T_gps_to_map,point = robot_origo_in_GPS_frame)
             rx,ry = r_pos[0],r_pos[1]
-            #print('r_vec', r_vec)
-            #print('yaw_angle',yaw_angle)
-            #print('gpsx,gpsy', gpsx,gpsy)
-            #print('robot origo',robot_origo_in_GPS_frame)
-            #print('rpos',r_pos)
             
             #find closest GT point
             closest_point_ind = closest_point(rx,ry,gt_x,gt_y)
         
             gt_line_point, gt_line_vec = line_to_next_point(closest_point_ind, gt_x,gt_y) #naive way - point to point
-            #r_point, r_vec = line_fit_from_points(ind,r_pos_x,r_pos_y,forward_window = 25, backward_window = 25)
-
+            
             angular_offset = angle_between_lines(gt_line_vec,r_vec)
-
             lateral_offset = signed_distance_point_to_line([rx,ry],gt_line_point,gt_line_vec)
             
             #Append to list for plotting and saving
@@ -176,7 +133,7 @@ def compute_and_save_robot_offsets(input_dir = os.path.join('.','output'),
 if __name__ == '__main__':
     input_dir = os.path.join('.','output/position_data')
     output_dir = os.path.join('.','output/position_data')
-    rec_prefix = '20191010_L3_S_morning_slaloam'
+    rec_prefix = '20191010_L4_N'
     debug = False
     visualize = True
 

auto_label/geometric_utilities.py
@@ -17,9 +17,10 @@ def angle_between_lines(v0,v1):
 def angle_between_vectors(v0,v1):
     # Input: unit vectors in 2D
     # Compute angle between vectors (with direction) Output range is from -pi to pi
-    y = v1[1]-v0[1]
-    x = v1[0]-v0[0]
-    return math.atan2(y,x)
+    sin_theta = np.cross(v0,v1)
+    cos_theta = np.dot(v0,v1)
+    return math.atan2(sin_theta,cos_theta)
+
 
 def signed_distance_point_to_line(point, line_point, line_vector):
     #Input: 2D points, 2D unit vector

auto_label/test_geometric_utilities.py
@@ -13,6 +13,11 @@ def vector2():
     return vector/np.linalg.norm(vector)
 
 @pytest.fixture
+def vector3():
+    vector = [np.sqrt(3),1]
+    return vector/np.linalg.norm(vector)
+
+@pytest.fixture
 def vector1_backwards():
     vector = [-1,-1]
     return vector/np.linalg.norm(vector)
@@ -94,27 +99,52 @@ def test_direction_same_vector(vector1):
 
 #Angle
 def test_angle_negative_correct_sign(vector1, vector2):
-    angle = geometric_utilities.angle_between_vectors(vector1,vector2)
+    angle = geometric_utilities.angle_between_lines(vector1,vector2)
     assert angle < 0
 
 def test_angle_positive_correct_sign(vector1, vector2):
-    angle = geometric_utilities.angle_between_vectors(vector2,vector1)
+    angle = geometric_utilities.angle_between_lines(vector2,vector1)
     assert angle > 0
 
 def test_angle_both_negative_correct_sign(vector1_negative, vector2_negative):
-    angle = geometric_utilities.angle_between_vectors(vector1_negative,vector2_negative)
+    angle = geometric_utilities.angle_between_lines(vector1_negative,vector2_negative)
     assert angle > 0
 
 def test_angle_positive_negative_correct_sign(vector1, vector2_negative):
-    angle = geometric_utilities.angle_between_vectors(vector1,vector2_negative)
+    angle = geometric_utilities.angle_between_lines(vector1,vector2_negative)
     assert angle < 0
 def test_angle_negative_correct_value(vector1, xvector):
-    angle = geometric_utilities.angle_between_vectors(vector1,xvector)
+    angle = geometric_utilities.angle_between_lines(vector1,xvector)
     assert angle == approx(-np.pi/4)
 
 def test_angle_positive_correct_value(vector1, xvector):
-    angle = geometric_utilities.angle_between_vectors(xvector,vector1)
+    angle = geometric_utilities.angle_between_lines(xvector,vector1)
     assert angle == approx(np.pi/4)
+
+def test_angle_between_vector_both_lower_right(vector1,vector3):
+    angle = geometric_utilities.angle_between_vectors(vector1,vector3)
+    assert angle == approx(-0.261799388)
+
+def test_angle_between_vector_lower_right(vector3,xvector):
+    print('vector',vector3)
+    angle = geometric_utilities.angle_between_vectors(vector3,xvector)
+    assert angle == approx(-0.5235987755982988)
+
+def test_angle_between_vector_upper_right(vector3,xvector):
+    print('vector',vector3)
+    angle = geometric_utilities.angle_between_vectors(xvector,vector3)
+    assert angle == approx(0.5235987755982988)
+
+def test_angle_between_vector_upper_left(vector3,xvector):
+    print('vector',vector3)
+    angle = geometric_utilities.angle_between_vectors(xvector,vector3*[-1,1])
+    assert angle == approx(0.5235987755982988+np.pi/2)
+
+def test_angle_between_vector_upper_left(vector3,xvector):
+    print('vector',vector3)
+    angle = geometric_utilities.angle_between_vectors(xvector,-vector3)
+    assert angle == approx(0.5235987755982988-np.pi)
+
 
 #Distance
 def test_signed_distance_negative_correct_sign(point, origo, vector1):